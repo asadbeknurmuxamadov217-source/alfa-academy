895aaabab19fc3307987f43332b453d7c0a5e7c6
asadbeknurmuxamadov217 <asadbeknurmuxamadov217@gmail.com>
2026-08-05 13:18:18 +0300

    Use safe_file_url custom template filter to catch all ValueError exceptions in template rendering

--- students/templatetags/custom_filters.py
+++ students/templatetags/custom_filters.py
@@ -7,3 +7,13 @@ def dict_key(d, key):
     if not isinstance(d, dict):
         return None
     return d.get(key)
+
+@register.filter(name='safe_file_url')
+def safe_file_url(field):
+    if not field:
+        return ''
+    try:
+        return field.url
+    except Exception:
+        return ''
+
